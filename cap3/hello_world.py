db9a4dc94bf1f89b1b25ab66b338f19a8ef7f948
Léo Ciardi <leolopes.ciardi04@gmail.com>
2020-12-09 21:54:07 -0300

    adding hello_world.py

---
+++ cap3/hello_world.py
@@ -0,0 +1,20 @@
+'''
+Este é seu primeiro script em python!
+Este programa colocará na tela a mensagem
+"Hello World!", que significa "Olá Mundo!".
+Toda vez que estiver algo escrito entre
+3 aspas ou na frente de um #, isso será
+considerado um COMENTÁRIO, comentários não
+são lidos pelo interpretador do python
+então nele você pode escrever o que quiser
+que não será considerado código.
+Não se esqueça de escrever o script você mesmo!!!
+'''
+
+print('Hello World!')
+
+# o print é um função que imprime/escreve algo na tela
+# ele escreverá o que está nos parênteses
+# tudo que estiver entre aspas será considerado uma STRING
+# o tipo de dado string ou str representa um texto, veremos
+# melhor isso mais para frente!
