cbc5c782013d8be47639bacfea697adeab84561d
Léo Ciardi <leolopes.ciardi04@gmail.com>
2021-02-01 16:37:15 -0300

    updating hello_world.py

--- cap3/hello_world.py
+++ cap3/hello_world.py
@@ -1,31 +1,38 @@
 """
 Este é seu primeiro script em python!
-Este programa colocará na tela a mensagem
+Este programa exibirá na tela a mensagem
 "Hello World!", que significa "Olá Mundo!".
 Toda vez que estiver algo escrito entre
-3 aspas ou na frente de um #, isso será
+3 aspas ou na frente de um #, será
 considerado um COMENTÁRIO, comentários não
 são lidos pelo interpretador do python
 então nele você pode escrever o que quiser
 que não será considerado código.
-Não se esqueça de escrever o script você mesmo!!!
+Não se esqueça de reescrever o script você mesmo!!!
 """
 
 print('Hello World!')
 
-# o print é um função que imprime/escreve algo na tela
+# o print é um função que escreve algo na tela
 # ele escreverá o que está nos parênteses
 # tudo que estiver entre aspas será considerado uma STRING
 # o tipo de dado string ou str representa um texto, veremos
 # melhor isso mais para frente!
 
 # com o print, você também pode escrever mais de uma coisa, apenas separando as
-# em vírgulas
+# em vírgulas, ou pelo sinal de mais, caso queira juntá-las ou somá-las
 
 print('Ebook', 'Python', 'Dominando Python Módulo 1')
 
+print('Pada' + 'ria')
+
+# o código a seguir resultará em um erro, pois não é possível
+# somar um número a um texto
+
+print(1 + 'olá')
+
 # DESAFIOS
 # DESAFIO 1: criar um script que escreverá na tela "Python é muito legal"
-# DESAFIO 2: criar um script que escreverá "O Python é "top""
+# DESAFIO 2: criar um script que escreverá "O Python é "top"" (cuidado com a pegadinha das aspas)
 # DESAFIO 3: criar um script escrevendo o que você quiser,
-# porém colocando comentários explicativos em seu código
+# porém colocando comentários explicativos em cada comando do seu código
